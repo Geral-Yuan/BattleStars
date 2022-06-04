5e38c83f1d5b0eeb6e49aea37966d4a9df9282c8
Your NameWang <wangyijun_1121@sjtu.edu.cn>
2022-06-04 15:33:14 +0800

    updatePaddle fixed

--- src/Milestone2.elm
+++ src/Milestone2.elm
@@ -1,7 +1,7 @@
 module Milestone2 exposing (..)
 
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta, onKeyDown)
+import Browser.Events exposing (onAnimationFrameDelta, onKeyDown, onKeyUp)
 import Debug exposing (toString)
 import Html exposing (..)
 import Html.Attributes as HtmlAttr exposing (..)
@@ -9,6 +9,7 @@ import Html.Events exposing (keyCode)
 import Json.Decode as Decode
 import String exposing (left)
 import Svg exposing (Svg)
+import Time
 import Svg.Attributes as SvgAttr
 
 
@@ -16,7 +17,6 @@ type alias Scoreboard =
     { player_score : Int
     , player_lives : Int
     }
-
 
 type Dir
     = Left
@@ -36,6 +36,7 @@ type alias Paddle =
     , dir : Dir
     , height : Float
     , width : Float
+    , speed : Float
     }
 
 
@@ -63,7 +64,7 @@ type alias Model =
 
 
 type Msg
-    = Key Dir
+    = Key Dir Bool
     | Key_None
     | Tick Float
 
@@ -93,8 +94,8 @@ init _ =
 initModel : Model
 initModel =
     { list_brick = initBrick ( 5, 10 ) 1 10 --one life for each brick; 10 points for each brick
-    , paddle = Paddle ( 50, 90 ) Left 2 15
-    , ball = Ball ( 50, 50 ) 1.5 (pi / 4) 0.3 --initial theta = pi/4; --initial speed = 1, adjust it if too fast or slow!!
+    , paddle = { pos =( 500, 900 ), dir = Still, height = 20, width = 150, speed = 500}
+    , ball = { pos = ( 500, 500 ), radius = 15, theta = (pi / 4), speed = 3}   --initial theta = pi/4; --initial speed = 1, adjust it if too fast or slow!!
     , time = 0
     , scoreboard = initScoreboard 3 --three lives for a player
     }
@@ -134,222 +135,170 @@ update msg model =
     ( model
     , Cmd.none
     )
-        --|> updateBrick msg
         |> updatePaddle msg
-        |> updateBall msg
+--        |> updateBall msg
+--        |> updateBrick msg
 
 
 updatePaddle : Msg -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
 updatePaddle msg ( model, cmd ) =
-    let
-        prev_paddle =
-            model.paddle
-
-        prev_x =
-            Tuple.first prev_paddle.pos
-
-        prev_y =
-            Tuple.second prev_paddle.pos
-
-        new_model =
-            updatePaddleDir model msg
-
-        new_paddle =
-            new_model.paddle
-
-        new_dir =
-            new_paddle.dir
-    in
     case msg of
-        Tick _ ->
-            --There is a problem wtih this code because msg Dir nested within Tick
-            case new_dir of
-                Left ->
-                    if prev_x < 0 then
-                        ( model, Cmd.none )
-
-                    else
-                        ( { new_model | paddle = { new_paddle | pos = ( prev_x - 0.3, prev_y ) } }, Cmd.none )
-
-                Right ->
-                    if prev_x > 85 then
-                        --condition to prevent paddle from moving out of boundary
-                        ( model, Cmd.none )
-
-                    else
-                        ( { new_model | paddle = { new_paddle | pos = ( prev_x + 0.3, prev_y ) } }, Cmd.none )
-
-                Still ->
-                    ( { new_model | paddle = { new_paddle | pos = ( prev_x, prev_y ) } }, Cmd.none )
-
+        Tick elapse ->
+            ({ model | paddle =
+                       movePaddle model.paddle (elapse/1000)  
+            },cmd)
+        Key dir on->
+            ({ model | paddle =
+                        updatePaddleDir model.paddle dir on
+            },cmd)
         _ ->
-            ( new_model, Cmd.none )
+            (model,cmd)
 
 
-updatePaddleDir : Model -> Msg -> Model
-updatePaddleDir model msg =
-    let
-        prev_paddle =
-            model.paddle
-    in
-    case msg of
-        Key Left ->
-            { model | paddle = { prev_paddle | dir = Left } }
-
-        Key Right ->
-            { model | paddle = { prev_paddle | dir = Right } }
-
-        Key_None ->
-            { model | paddle = { prev_paddle | dir = Still } }
-
-        _ ->
-            model
-
-
-
--- { model | paddle = { prev_paddle | dir = Still } }
-
-
-updateBall : Msg -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
-updateBall msg ( model, cmd ) =
-    let
-        prev_ball =
-            model.ball
-
-        prev_x =
-            Tuple.first prev_ball.pos
-
-        prev_y =
-            Tuple.second prev_ball.pos
-
-        prev_theta =
-            prev_ball.theta
-
-        prev_speed =
-            prev_ball.speed
-
-        new_ball =
-            { prev_ball | pos = ( prev_x + cos prev_theta * prev_speed, prev_y + sin prev_theta * prev_speed ) }
-
-        new_model =
-            checkBounce { model | ball = new_ball }
-    in
-    case msg of
-        Tick _ ->
-            ( new_model, cmd )
-
-        _ ->
-            ( new_model, cmd )
-
-
-checkBounce : Model -> Model
-checkBounce model =
-    checkBouncePaddle model
-        |> checkBounceScreen
-
-
-
--- |> checkBounceBrick
-
-
-checkBouncePaddle : Model -> Model
-checkBouncePaddle model =
-    let
-        ( x, y ) =
-            model.ball.pos
-
-        r =
-            model.ball.radius
-    in
-    if y - r == Tuple.second model.paddle.pos then
-        if x >= Tuple.first model.paddle.pos && x <= Tuple.first model.paddle.pos + model.paddle.width then
-            let
-                prev_ball =
-                    model.ball
-
-                prev_theta =
-                    prev_ball.theta
-
-                new_ball =
-                    { prev_ball | theta = (2 * pi) - prev_theta }
-            in
-            { model | ball = new_ball }
-
-        else
-            model
-
+updatePaddleDir : Paddle -> Dir -> Bool -> Paddle
+updatePaddleDir paddle dir on =
+    if on then
+        {paddle | dir = dir}
     else
-        model
+        {paddle | dir = Still}
+        
+
+movePaddle : Paddle -> Float -> Paddle
+movePaddle paddle dt=
+    case paddle.dir of
+        Still ->
+            paddle
+        _ -> if (legalMovePaddle paddle) then
+                { paddle | pos = newPaddlePos paddle.pos paddle.dir (paddle.speed * dt )}
+             else
+                paddle
+legalMovePaddle : Paddle -> Bool
+legalMovePaddle { pos, dir, width} =
+    case dir of 
+        Left ->
+            if ( Tuple.first pos - width/2 <= 0) then
+                False
+            else
+                True
+        Right ->
+            if ( Tuple.first pos + width/2 >= 10 * brickwidth) then  --the right bound of the game screen
+                False
+            else
+                True
+        _ -> False
+
+newPaddlePos : (Float, Float) -> Dir -> Float -> (Float, Float)
+newPaddlePos (px,py) dir ds=
+    case dir of
+        Left ->
+            (px - ds , py)
+        Right ->
+            (px + ds , py)
+        _ -> 
+            (px,py)
 
 
-checkBounceScreen : Model -> Model
-checkBounceScreen model =
-    let
-        prev_ball =
-            model.ball
 
-        ( x, y ) =
-            prev_ball.pos
+-- updateBall : Msg -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
+-- updateBall msg ( model, cmd ) =
+--     let
+--         prev_ball =
+--             model.ball
 
-        r =
-            prev_ball.radius
+--         prev_x =
+--             Tuple.first prev_ball.pos
 
-        prev_theta =
-            prev_ball.theta
-    in
-    if x + r >= 100 then
-        { model | ball = { prev_ball | theta = pi - prev_theta } }
+--         prev_y =
+--             Tuple.second prev_ball.pos
 
-    else if x - r <= 0 then
-        { model | ball = { prev_ball | theta = 3 * pi - prev_theta } }
+--         prev_theta =
+--             prev_ball.theta
 
-    else if y - r <= 0 then
-        { model | ball = { prev_ball | theta = 2 * pi - prev_theta } }
+--         prev_speed =
+--             prev_ball.speed
 
-    else
-        model
+--         new_ball =
+--             { prev_ball | pos = ( prev_x + cos prev_theta * prev_speed, prev_y + sin prev_theta * prev_speed ) }
 
-
-
--- Previous updatePaddle without Tick for your reference
--- updatePaddle : Msg -> ( Model, Cmd Msg ) -> ( Model, Cmd Msg )
--- updatePaddle msg ( model, cmd ) =
+--         new_model =
+--             checkBounce { model | ball = new_ball }
+--     in
 --     case msg of
---         Key Left ->
---             let
---                 oldPaddle =
---                     model.paddle
---                 newPaddle =
---                     { oldPaddle
---                         | pos = paddlePos oldPaddle.pos Left
---                     }
---             in
---             ( { model | paddle = newPaddle }, cmd )
---         Key Right ->
---             let
---                 oldPaddle =
---                     model.paddle
---                 newPaddle =
---                     { oldPaddle
---                         | pos = paddlePos oldPaddle.pos Right
---                     }
---             in
---             ( { model | paddle = newPaddle }, cmd )
+--         Tick _ ->
+--             ( new_model, cmd )
+
 --         _ ->
---             ( model, cmd )
--- paddlePos : ( Float, Float ) -> Dir -> ( Float, Float )
--- paddlePos ( oldx, y ) dir =
---     case dir of
---         Left ->
---             if oldx - 8 < 0 then
---                 ( 0, y )
---             else
---                 ( oldx - 8, y )
---         Right ->
---             if oldx + 8 > 85 then
---                 ( 85, y )
---             else
---                 ( oldx + 8, y )
+--             ( new_model, cmd )
+
+
+-- checkBounce : Model -> Model
+-- checkBounce model =
+--     checkBouncePaddle model
+--         |> checkBounceScreen
+
+
+
+-- -- |> checkBounceBrick
+
+
+-- checkBouncePaddle : Model -> Model
+-- checkBouncePaddle model =
+--     let
+--         ( x, y ) =
+--             model.ball.pos
+
+--         r =
+--             model.ball.radius
+--     in
+--     if y - r == Tuple.second model.paddle.pos then
+--         if x >= Tuple.first model.paddle.pos && x <= Tuple.first model.paddle.pos + model.paddle.width then
+--             let
+--                 prev_ball =
+--                     model.ball
+
+--                 prev_theta =
+--                     prev_ball.theta
+
+--                 new_ball =
+--                     { prev_ball | theta = (2 * pi) - prev_theta }
+--             in
+--             { model | ball = new_ball }
+
+--         else
+--             model
+
+--     else
+--         model
+
+
+-- checkBounceScreen : Model -> Model
+-- checkBounceScreen model =
+--     let
+--         prev_ball =
+--             model.ball
+
+--         ( x, y ) =
+--             prev_ball.pos
+
+--         r =
+--             prev_ball.radius
+
+--         prev_theta =
+--             prev_ball.theta
+--     in
+--     if x + r >= 100 then
+--         { model | ball = { prev_ball | theta = pi - prev_theta } }
+
+--     else if x - r <= 0 then
+--         { model | ball = { prev_ball | theta = 3 * pi - prev_theta } }
+
+--     else if y - r <= 0 then
+--         { model | ball = { prev_ball | theta = 2 * pi - prev_theta } }
+
+--     else
+--         model
+
+
 ----------
 -- VIEW
 ----------
@@ -357,12 +306,12 @@ checkBounceScreen model =
 
 brickwidth : Float
 brickwidth =
-    10
+    100
 
 
 brickheight : Float
 brickheight =
-    5
+    40
 
 
 viewBricks : Brick -> Svg msg
@@ -373,10 +322,10 @@ viewBricks brick =
     in
     -- add if condition of bricks existence later
     Svg.rect
-        [ SvgAttr.width (toString brickwidth ++ "%")
-        , SvgAttr.height (toString brickheight ++ "%")
-        , SvgAttr.x (toString ((col - 1) * brickwidth) ++ "%")
-        , SvgAttr.y (toString ((row - 1) * brickheight + 5) ++ "%")
+        [ SvgAttr.width (toString brickwidth ++ "")
+        , SvgAttr.height (toString brickheight ++ "")
+        , SvgAttr.x (toString ((col - 1) * brickwidth) ++ "")
+        , SvgAttr.y (toString ((row - 1) * brickheight + 50) ++ "")
         , SvgAttr.fill "rgb(104,91,209)"
         , SvgAttr.stroke "white"
         ]
@@ -448,18 +397,18 @@ view model =
             (List.map viewBricks model.list_brick
                 ++ -- draw paddle
                    Svg.rect
-                    [ SvgAttr.width (toString model.paddle.width ++ "%")
-                    , SvgAttr.height (toString model.paddle.height ++ "%")
-                    , SvgAttr.x (toString (Tuple.first model.paddle.pos) ++ "%")
-                    , SvgAttr.y (toString (Tuple.second model.paddle.pos) ++ "%")
+                    [ SvgAttr.width (toString model.paddle.width ++ "")
+                    , SvgAttr.height (toString model.paddle.height ++ "")
+                    , SvgAttr.x (toString ( Tuple.first (model.paddle.pos ) - (model.paddle.width) / 2) ++ "")
+                    , SvgAttr.y (toString (Tuple.second model.paddle.pos + (model.paddle.height) / 2) ++ "")
                     , SvgAttr.fill "rgb(30,144,255)"
                     ]
                     []
                 :: -- draw ball
                    [ Svg.circle
-                        [ SvgAttr.cx (toString (Tuple.first model.ball.pos) ++ "%")
-                        , SvgAttr.cy (toString (Tuple.second model.ball.pos) ++ "%")
-                        , SvgAttr.r (toString model.ball.radius ++ "%")
+                        [ SvgAttr.cx (toString (Tuple.first model.ball.pos) ++ "")
+                        , SvgAttr.cy (toString (Tuple.second model.ball.pos) ++ "")
+                        , SvgAttr.r (toString model.ball.radius ++ "")
                         , SvgAttr.fill "rgb(0,0,128)"
                         ]
                         []
@@ -480,19 +429,21 @@ view model =
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.batch
-        [ onAnimationFrameDelta Tick
-        , onKeyDown (Decode.map key keyCode)
+        [ 
+        onAnimationFrameDelta Tick
+        , onKeyUp (Decode.map (key False) keyCode)
+        , onKeyDown (Decode.map (key True) keyCode)
         ]
 
 
-key : Int -> Msg
-key keycode =
+key :Bool -> Int -> Msg
+key on keycode =
     case keycode of
         37 ->
-            Key Left
+            Key Left on
 
         39 ->
-            Key Right
+            Key Right on
 
         _ ->
             Key_None
