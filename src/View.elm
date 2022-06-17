ae1d7ca4f669ebe6d923404867125e040eac7193
Your NameWang <wangyijun_1121@sjtu.edu.cn>
2022-06-17 19:59:51 +0800

    fix paddleBounce; add fancy cover and ball appearance

--- src/View.elm
+++ src/View.elm
@@ -146,9 +146,10 @@ viewPlaying model =
              ]
                 ++ viewLives model
                 ++ List.map viewMonsters model.monster_list
-                ++ List.map viewCover model.monster_list
-                ++ List.map viewBall model.ball_list
-                ++ [ viewPaddle model, viewBoss model.boss, viewBossCover model.boss ]
+                ++ (List.concat (List.map viewCover model.monster_list))
+                ++ (List.concat(List.map viewBall model.ball_list))
+                ++ [ viewPaddle model, viewBoss model.boss] 
+                ++ viewBossCover model.boss
             )
         , viewScore model
         ]
